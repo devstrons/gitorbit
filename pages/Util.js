f664358a32bf94f528ce6c3b97f4144cfb914d28
CuriosBasant <basantbrpl@gmail.com>
2022-10-09 01:26:22 +0530

    feat: :fire: delete whole project

--- pages/Util.js
+++
@@ -1,9 +0,0 @@
-export function removeDuplicates(arr)
-{
-    const obj = {}
-    for (const item of arr)
-    { // for each element in the array
-        obj[ item.id ] = item // removing dulpicate items
-    }
-    return Object.values(obj)
-}
